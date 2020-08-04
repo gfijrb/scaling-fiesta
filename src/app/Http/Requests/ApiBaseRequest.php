61a7a877b2565e10f51cce1c03ea9bdb1db7905c
piron <gfijr@i.ua>
2020-08-04 17:55:17 +0300

    add only json resp rules

---
+++ src/app/Http/Requests/ApiBaseRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Http\Request;
+
+class ApiBaseRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+
+    public function expectsJson()
+    {
+        return true;
+    }
+
+    public function wantsJson()
+    {
+        return true;
+    }
+
+}
